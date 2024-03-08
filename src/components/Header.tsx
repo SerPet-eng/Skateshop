e984c62faf05407acedb36adea61dd9a33b59692
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-03-08 19:10:37 +0800

    Setting Up Header🧭👷‍♂️
     * Styling up the header section with the navigation bar
    
     - The styling was start with mobile version.
    
    * Adding default font throughout the body.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,31 @@
+import Logo from '../../images/main_AxisRacoon-removebg.png';
+import Menu from '../../images/icon_Menu.svg';
+
 export default function Header() {
   return (
     <>
       <header className="header">
-        <img src="" alt="" />
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Product</li>
-            <li>Gallery</li>
-            <li>About</li>
-            <li>Contact</li>
+        <img
+          src={Logo}
+          alt="Logo of Axis Skateshop 'The Raccoon'"
+          className="logo"
+        />
+        <button className="menu_button">
+          <img src={Menu} alt="Menu Button" className="menu_icon" />
+        </button>
+        <nav className="nav">
+          <ul className="nav_menu">
+            <li className="nav_list">Home</li>
+            <li className="nav_list">Product</li>
+            <li className="nav_list">Gallery</li>
+            <li className="nav_list">About</li>
+            <li className="nav_list">Contact</li>
           </ul>
+          <div className="nav_buttons">
+            <button className="nav_login">Login</button>
+            <button className="nav_register">Register</button>
+          </div>
         </nav>
-        <div className="nav_buttons">
-          <button>Login</button>
-          <button>Register</button>
-        </div>
       </header>
     </>
   );
