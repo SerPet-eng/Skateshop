c509cfb6dcb30f3c889d9d2cf67d6a89a15fbd44
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-03-11 17:00:05 +0800

    Styling Up Sponsor Section✨👷‍♂️
      - Adding all the sponsor icon for the Sponsor Section
     - Styling Up the Sponsor Section

--- src/components/Section/Sponsors.tsx
+++ src/components/Section/Sponsors.tsx
@@ -1,7 +1,21 @@
+import Palaboy from '../../../images/sponsor_PalaboyLogo-removebg.png';
+import Axis from '../../../images/main_AxisRacoon-removebg.png';
+import Tubero from '../../../images/sponsor_Tubero-removebg.png';
+import Corterie from '../../../images/sponsor_CorterieLogo-removebg.png';
+import NBGD from '../../../images/sponsor_NBGD-removebg.png';
+import Vendetta from '../../../images/sponsor_VendettaLogo-removebg.png';
+
 export default function Sponsors() {
   return (
     <>
-      <h1>Sponsor Test</h1>
+      <figure className="sponsor">
+        <img src={Palaboy} alt="Palaboy Icon" className="sponsor_icon" />
+        <img src={Axis} alt="Axis Icon" className="sponsor_icon" />
+        <img src={Tubero} alt="Tubero Icon" className="sponsor_icon" />
+        <img src={Corterie} alt="Corterie Icon" className="sponsor_icon" />
+        <img src={NBGD} alt="NBGD Icon" className="sponsor_icon" />
+        <img src={Vendetta} alt="Vendetta Icon" className="sponsor_icon" />
+      </figure>
     </>
   );
 }
