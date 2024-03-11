42ee3de44060f386d34d56f5d00fee941ada7193
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-03-11 16:41:35 +0800

    Styling Quotes✨👷‍♂️
     - Styling Up Quotes Section.
     - Adding the necessary images and quotes that needs to be in this section.

--- src/components/Section/Quotes.tsx
+++ src/components/Section/Quotes.tsx
@@ -1,7 +1,30 @@
+import ImageOne from '../../../images/skateScene1.jpg';
+import ImageThree from '../../../images/skateScene2.jpg';
+import ImageTwo from '../../../images/skateScene3.jpg';
+
 export default function Quotes() {
   return (
     <>
-      <h1>Quotes Test</h1>
+      <section className="quote">
+        <div className="quote_photos">
+          <img src={ImageOne} alt="Skate Scenery The Streets" />
+          <img
+            src={ImageTwo}
+            alt="Skate Scenery Where Skater are doing Ollie"
+          />
+          <img src={ImageThree} alt="Skate Scenery of A Bunch of Skater" />
+        </div>
+        <article className="quote_article">
+          <h2 className="quote_title">Don&apos;t Worry Keep Pushing</h2>
+          <p className="quote_description">
+            &quot;Do what you love and try not to look at what other people
+            occupy themselves with. Most people seem restless and bounce around
+            too much to focus or even pay attention enough to themselves to
+            figure out exactly what they really do love, as opposed to what the
+            people that surround them are doing.&quot; ~ Rodney Mullen
+          </p>
+        </article>
+      </section>
     </>
   );
 }
