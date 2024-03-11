0d7f34b2348fc11ee534401a4ad4f316b9cc83fe
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-03-11 16:18:56 +0800

    Styling About Section✨👷‍♂️
     - Adding style to About Section

--- src/components/Body/About.tsx
+++ src/components/Body/About.tsx
@@ -1,7 +1,24 @@
+import Icon from '../../../images/main_AxisRacoon-removebg.png';
+import Sample from '../../../images/Video Player.png';
+
 export default function About() {
   return (
     <>
-      <h1>About Test</h1>
+      <section className="about">
+        <article className="about_article">
+          <h2 className="about_title">About</h2>
+          <p className="about_description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            itaque ratione dolorem molestias accusamus repellat tenetur atque
+            dolore qui nostrum?
+          </p>
+          <button className="about_button">Click Here</button>
+        </article>
+        <div className="about_media">
+          <img src={Icon} alt="Mr. Raccoon" className="about_icon" />
+          <img src={Sample} alt="A Sample Video" className="about_video" />
+        </div>
+      </section>
     </>
   );
 }
