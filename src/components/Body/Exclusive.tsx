c76f6ea357040d57b7391fbf1f43f2a9bd1234de
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-03-11 14:05:13 +0800

    Creating Exclusive Section🛠️👷‍♂️
     - Adding styles to the Exclusive Section
     - Also add specific colors to the button of Exclusive section

--- src/components/Body/Exclusive.tsx
+++ src/components/Body/Exclusive.tsx
@@ -1,7 +1,21 @@
+import Image from '../../../images/exclusiveProduct1-Red.png';
+
 export default function Exclusive() {
   return (
     <>
-      <h1>Exclusive Test</h1>
+      <section className="exclusive">
+        <h1>Exclusive</h1>
+        <div className="exclusive_item">
+          <img src={Image} alt="Exclusive Products" />
+          <h2>Lorem, ipsum dolor.</h2>
+          <p className="exclusive_description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            aspernatur sunt laboriosam quam explicabo tenetur cum repellat fugit
+            corrupti unde.
+          </p>
+          <button className="exclusive_button">Click Here</button>
+        </div>
+      </section>
     </>
   );
 }
