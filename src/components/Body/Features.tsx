d37526fb6f5d5a58657f6725f829f1b789edda54
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-03-11 11:31:51 +0800

    Creating Features Section👷‍♂️📝
     - Making the feature section:
     * Adding product panel sample for placeholder
     * Adding borders and spacing
     * Creating a style file for the feature section

--- src/components/Body/Features.tsx
+++ src/components/Body/Features.tsx
@@ -1,7 +1,18 @@
+import Sample from '../../../images/Product Panel.png';
+
 export default function Features() {
   return (
     <>
-      <h1>Features Test</h1>
+      <section className="features">
+        <h1 className="features_title">Feature Products</h1>
+        <div className="product_section">
+          <img src={Sample} alt="Product Sample" className="product" />
+          <img src={Sample} alt="Product Sample" className="product" />
+          <img src={Sample} alt="Product Sample" className="product" />
+          <img src={Sample} alt="Product Sample" className="product" />
+          <img src={Sample} alt="Product Sample" className="product" />
+        </div>
+      </section>
     </>
   );
 }
